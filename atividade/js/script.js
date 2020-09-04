eaed39635808a3a8121e59d1813576997693cef6
LauraLuz <lauraluzcabral@gmail.com>
2020-09-04 12:03:34 -0300

    exercicio 4 js
    funções

--- atividade/js/script.js
+++ atividade/js/script.js
@@ -5,13 +5,19 @@
 function estiloClassico() {
     document.getElementById("declaracao").style.fontStyle = "italic";
     document.getElementById("declaracao").style.color = "blue";
-    document.getElementById("declaracao").style.color = "blue";
+    document.getElementById("declaracao").style.backgroundColor = "blue";
 }
 
 function estiloModerno() {
     document.getElementById("declaracao").style.fontStyle = "oblique";
+    document.getElementById("declaracao").style.color = "blue";
+    document.getElementById("declaracao").style.backgroundColor = "blue";
+
 }
 
 function estiloEsporte() {
     document.getElementById("declaracao").style.fontStyle = "normal";
+    document.getElementById("declaracao").style.color = "blue";
+    document.getElementById("declaracao").style.backgroundColor = "blue";
+
 }
