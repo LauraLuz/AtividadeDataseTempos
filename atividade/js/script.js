00fe9cf747a5ae8824661d6bba51ad2ae6012546
LauraLuz <lauraluzcabral@gmail.com>
2020-09-04 11:58:16 -0300

    Exercicio 4 js

--- atividade/js/script.js
+++ atividade/js/script.js
@@ -4,4 +4,14 @@
 
 function estiloClassico() {
     document.getElementById("declaracao").style.fontStyle = "italic";
+    document.getElementById("declaracao").style.color = "blue";
+    document.getElementById("declaracao").style.color = "blue";
+}
+
+function estiloModerno() {
+    document.getElementById("declaracao").style.fontStyle = "oblique";
+}
+
+function estiloEsporte() {
+    document.getElementById("declaracao").style.fontStyle = "normal";
 }
